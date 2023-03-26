7f00595105ea75fbe85b31182a72819c1faabab4
yourseulah <imseulahlee@gmail.com>
2023-03-25 18:12:18 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div className='App'>
+    <div>
+      <EnrolmentForm />
+    </div>
+    <div>
+      <EnrolForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import EnrolmentForm from './components/EnrolmentForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EnrolmentForm,
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+  },
+
+  mounted() {
+
+  }
+
+
+}
+</script>
+
+<style></style>
---
+++ src/components/EnrolmentForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <form className='enrolForm'>
+            <h1>Student Details</h1>
+            <label>First name:</label>
+            <input type="text" name="fname" />
+            <br />
+            <label>Last name:</label>
+            <input type="text" name="lname" />
+            <br />
+            <br />
+            <input type='submit' value='Submit' />
+            <br />
+        </form>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'EnrollmentForm',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    }
+
+}
+</script>
+
+<style scoped></style>
